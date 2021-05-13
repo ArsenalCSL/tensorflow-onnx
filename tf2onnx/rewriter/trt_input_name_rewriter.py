6ab51e3d740c784782be99410bbaf7d90979680d
Matthias Langer <bashimao@users.noreply.github.com>
2021-05-13 11:41:22 +0800

    Do not attempt to rename an input if its name does actually not change.

--- tf2onnx/rewriter/trt_input_name_rewriter.py
+++ tf2onnx/rewriter/trt_input_name_rewriter.py
@@ -8,23 +8,23 @@ import re
 
 
 def rewrite_input_name_for_trt(g, ops):
-    r = re.compile(r'.+(?=:0)')
-
     old_inputs = g.inputs.copy()
 
     # Craft new inputs with the name we want.
     for old_input in old_inputs:
         o = old_input.output[0]
-        n = r.search(o).group()
-        dt = g.get_dtype(o)
-        sh = g.get_shape(o)
-        g.add_graph_input(n, dt, sh)
+        n = o.rsplit(':', 1)[0]
+        if o != n:
+            dt = g.get_dtype(o)
+            sh = g.get_shape(o)
+            g.add_graph_input(n, dt, sh)
 
     # Rewire nodes to the new inputs and erase the old input.
     for old_input in old_inputs:
         o = old_input.output[0]
-        n = r.search(o).group()
-        g.replace_all_inputs(o, n)
-        g.remove_node(old_input.name)
+        n = o.rsplit(':', 1)[0]
+        if o != n:
+            g.replace_all_inputs(o, n)
+            g.remove_node(old_input.name)
 
     return g.get_nodes()
